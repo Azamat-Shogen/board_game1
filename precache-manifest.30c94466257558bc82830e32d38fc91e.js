37b5c2e3ebab45eeb87504a6357adff0c98ba139
baki-han <baki47hanma@gmail.com>
2020-09-27 14:51:12 -0500

    Updates

---
+++ precache-manifest.30c94466257558bc82830e32d38fc91e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112230070635e24c423ece34af26219b",
+    "url": "/board_game1/index.html"
+  },
+  {
+    "revision": "0cab8fa78ebd59fdfacb",
+    "url": "/board_game1/static/css/main.7c38bc6a.chunk.css"
+  },
+  {
+    "revision": "b889fe9d1190807d759b",
+    "url": "/board_game1/static/js/2.9ac50c35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/board_game1/static/js/2.9ac50c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cab8fa78ebd59fdfacb",
+    "url": "/board_game1/static/js/main.f5ff9028.chunk.js"
+  },
+  {
+    "revision": "2948728fddae6e752698",
+    "url": "/board_game1/static/js/runtime-main.b56bc5e9.js"
+  }
+]);
